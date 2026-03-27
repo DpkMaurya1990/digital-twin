aef50e739b697ca8f280ddd64883106a39121552
Deepak Maurya <deepak.mau2@gmail.com>
2026-03-27 09:54:16 +0530

    Updates S3 bucket

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -4,16 +4,20 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.0"
+      version = "~> 5.0" # Stable version for now
     }
+  }
+
+  # --- IS BLOCK KO ADD KARNA ZAROORI HAI ---
+  backend "s3" {
+    bucket         = "twin-terraform-state-211125631519"
+    key            = "global/s3/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "twin-terraform-locks"
+    encrypt        = true
   }
 }
 
 provider "aws" {
-  # Uses AWS CLI configuration (aws configure)
-}
-
-provider "aws" {
-  alias  = "ap_south_1"
-  region = "ap-south-1"
+  region = "ap-south-1" 
 }
