23db304e130b8b8f9d7308aa0934649b61bfd67a
Deepak Maurya <deepak.mau2@gmail.com>
2026-03-27 11:13:45 +0530

    Fixed the test environment

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,33 @@
 terraform {
-  backend "s3" {
-    # These values will be set by deployment scripts
-    # For local development, they can be passed via -backend-config
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0" # Stable version for now
+    }
   }
+
+  # --- IS BLOCK KO ADD KARNA ZAROORI HAI ---
+  backend "s3" {
+    bucket         = "twin-terraform-state-211125631519"
+    key            = "global/s3/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "twin-terraform-locks"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1" 
+}
+
+provider "aws" {
+  alias  = "ap_south_1"
+  region = "ap-south-1"
+}
+
+variable "github_repository" {
+  type = string
+  default = "DpkMaurya1990/digital-twin"
 }
